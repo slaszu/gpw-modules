406f3cf87689d07a77a8dbe8450b77139fda2fe2
slaszu <piotr.flasza@gmail.com>
2023-08-02 21:31:12 +0200

    module admin spring mvc

---
+++ admin/src/main/java/pl/slaszu/gpw/admin/AdminSpringApplication.java
@@ -0,0 +1,11 @@
+package pl.slaszu.gpw.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminSpringApplication.class, args);
+    }
+}
