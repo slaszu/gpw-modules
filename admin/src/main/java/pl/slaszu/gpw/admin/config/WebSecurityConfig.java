17a193be2e1ee590e5a7d283fe3982e6177a0428
slaszu <piotr.flasza@gmail.com>
2023-08-04 22:14:19 +0200

    oauth2 github

--- admin/src/main/java/pl/slaszu/gpw/admin/config/WebSecurityConfig.java
+++ admin/src/main/java/pl/slaszu/gpw/admin/config/WebSecurityConfig.java
@@ -1,14 +1,21 @@
 package pl.slaszu.gpw.admin.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +32,23 @@ public class WebSecurityConfig {
                 .loginPage("/login")
                 .permitAll()
             )
-            .logout((logout) -> logout.permitAll());
+            .logout((logout) -> logout.permitAll())
+            .oauth2Login().successHandler(new AuthenticationSuccessHandler() {
+
+                @Override
+                public void onAuthenticationSuccess(
+                    HttpServletRequest request,
+                    HttpServletResponse response,
+                    Authentication authentication
+                ) throws IOException, ServletException {
+
+                    Object principal = authentication.getPrincipal();
+
+                    //userService.processOAuthPostLogin(oauthUser.getEmail());
+
+                    response.sendRedirect("/list");
+                }
+            });
 
         return http.build();
     }
