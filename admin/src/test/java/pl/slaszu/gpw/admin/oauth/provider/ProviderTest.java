4bf9b76f2c09e704dee13d009d9314441f85e8cd
Piotr Flasza <piotr.flasza@enp.pl>
2023-10-20 12:04:02 +0200

    tests in admin module

---
+++ admin/src/test/java/pl/slaszu/gpw/admin/oauth/provider/ProviderTest.java
@@ -0,0 +1,13 @@
+package pl.slaszu.gpw.admin.oauth.provider;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProviderTest {
+
+    @Test
+    void getProviderImplementation() {
+        assertEquals(1,2);
+    }
+}
