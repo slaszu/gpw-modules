ce59214b007889bc702eb2cb975cc0790cd971ad
Piotr Flasza <piotr.flasza@enp.pl>
2023-10-20 14:58:57 +0200

    tests for admine module

---
+++ admin/src/test/java/pl/slaszu/gpw/admin/controller/SimpleControllerTest.java
@@ -0,0 +1,57 @@
+package pl.slaszu.gpw.admin.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SimpleControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void homePage() throws Exception {
+
+        mvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("<title>Home page</title>")));
+    }
+
+    @Test
+    void loginAuthPage() throws Exception {
+
+        mvc.perform(get("/login_oauth"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("<a href=\"/oauth2/authorization/google\">Google</a>")))
+            .andExpect(content().string(containsString("<a href=\"/oauth2/authorization/github\">GitHub</a>")));
+    }
+
+    @Test
+    void adminWithoutUserPage() throws Exception {
+
+        mvc.perform(get("/admin"))
+            .andExpect(status().isFound())
+            .andExpect(redirectedUrl("http://localhost/login_oauth"));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    void adminWithUserPage() throws Exception {
+
+        mvc.perform(get("/admin"))
+            .andExpect(status().isOk());
+    }
+}
--- admin/src/test/java/pl/slaszu/gpw/admin/oauth/provider/ProviderTest.java
+++ admin/src/test/java/pl/slaszu/gpw/admin/oauth/provider/ProviderTest.java
@@ -2,12 +2,25 @@ package pl.slaszu.gpw.admin.oauth.provider;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ProviderTest {
 
     @Test
     void getProviderImplementation() {
-        assertEquals(1,2);
+
+        Provider provider;
+        ProviderInterface providerImplementation;
+
+        provider = Provider.valueOf("GOOGLE");
+        providerImplementation = provider.getProviderImplementation();
+        assertEquals(GoogleProvider.class, providerImplementation.getClass());
+
+        provider = Provider.valueOf("GITHUB");
+        providerImplementation = provider.getProviderImplementation();
+        assertEquals(GithubProvider.class, providerImplementation.getClass());
+
+        assertThrows(IllegalArgumentException.class, () -> Provider.valueOf("NOTKNOW"));
     }
 }
