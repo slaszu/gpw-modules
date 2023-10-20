3afb286618cddf613c75a895984b9a863d9b8716
Piotr Flasza <piotr.flasza@enp.pl>
2023-10-20 15:08:21 +0200

    test & itegration test

--- admin/src/test/java/pl/slaszu/gpw/admin/controller/SimpleControllerIT.java
+++ admin/src/test/java/pl/slaszu/gpw/admin/controller/SimpleControllerIT.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class SimpleControllerTest {
+class SimpleControllerIT {
 
     @Autowired
     MockMvc mvc;
