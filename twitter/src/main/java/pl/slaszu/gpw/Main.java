5be1008768b812b07b3e315d965631680998842e
Piotr Flasza <piotr.flasza@enp.pl>
2023-11-21 10:43:29 +0100

    POC: add kotlin lib to project

--- twitter/src/main/java/pl/slaszu/gpw/Main.java
+++ twitter/src/main/java/pl/slaszu/gpw/Main.java
@@ -15,32 +15,42 @@ import java.net.URL;
 import java.nio.file.Paths;
 import java.util.List;
 
+import pl.slaszu.kotlin.Lib;
+
 public class Main {
     @SneakyThrows
     public static void main(String[] args) {
 
-        TwitterClient twitterClient = new TwitterClient(TwitterCredentials.builder()
-            .accessToken("-")
-            .accessTokenSecret("")
-            .apiKey("")
-            .apiSecretKey("")
-            .build());
+        Lib l = new Lib(23,7);
 
-        UploadMediaResponse uploadMediaResponse = twitterClient.uploadMedia(
-            new File("/home/.../gold.jpg"),
-            MediaCategory.TWEET_IMAGE
-        );
+        System.out.println(l);
 
-        TweetParameters.TweetParametersBuilder builder = TweetParameters.builder();
-        TweetParameters tweetContent = builder.text("test api").media(
-            TweetParameters.Media.builder().mediaIds(List.of(uploadMediaResponse.getMediaId())).build()
-        ).build();
+        System.out.println(l.sum());
 
+        System.out.println(l.sub());
 
-        Tweet tweet = twitterClient.postTweet(tweetContent);
-        System.out.println(tweet.getId());
-        System.out.println(tweet.getText());
-        System.out.println(tweet.getCreatedAt());
+//        TwitterClient twitterClient = new TwitterClient(TwitterCredentials.builder()
+//            .accessToken("-")
+//            .accessTokenSecret("")
+//            .apiKey("")
+//            .apiSecretKey("")
+//            .build());
+//
+//        UploadMediaResponse uploadMediaResponse = twitterClient.uploadMedia(
+//            new File("/home/.../gold.jpg"),
+//            MediaCategory.TWEET_IMAGE
+//        );
+//
+//        TweetParameters.TweetParametersBuilder builder = TweetParameters.builder();
+//        TweetParameters tweetContent = builder.text("test api").media(
+//            TweetParameters.Media.builder().mediaIds(List.of(uploadMediaResponse.getMediaId())).build()
+//        ).build();
+//
+//
+//        Tweet tweet = twitterClient.postTweet(tweetContent);
+//        System.out.println(tweet.getId());
+//        System.out.println(tweet.getText());
+//        System.out.println(tweet.getCreatedAt());
     }
 
 }
